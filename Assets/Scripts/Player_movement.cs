d15f72dc34b5506c58bee7b0ee36427c8340cb3b
Heliorage <mhaonguyen0707@gmail.com>
2025-06-06 11:28:58 +0700

    fix rotation + some improvement

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -7,8 +7,6 @@ public class Player_movement : MonoBehaviour
     public Rigidbody2D rb; // get the rigidbody of the player
     Vector2 movement; // movement input var
     public Weapon weapon;
-    Vector2 mouseposition;
-    public Camera cam;
  
     void Update()
     {
@@ -16,22 +14,15 @@ public class Player_movement : MonoBehaviour
 
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
-        if (Input.GetMouseButtonDown(0))
-        {
-            weapon.Fire();
-        }
-       mouseposition = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
-    
+
     private void FixedUpdate()
     {
         // applying movement to the rigidbody2D :
+
         movement.Normalize(); // normalizing movement so it doesn't go faster diagonally
-            rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime); 
-        Vector2 aimDirection = mouseposition - rb.position;
-        float aimangle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimangle;
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
     }
 
 
